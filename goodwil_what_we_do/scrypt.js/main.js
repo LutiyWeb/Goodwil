1105ffa85e55d4c9e369612d89a5e17bd0659790
Yuriy <Lutiy2501.com@gmail.com>
2022-04-28 21:36:04 +0300

    goodwil

---
+++ goodwil_what_we_do/scrypt.js/main.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+    $('.burger').click(function() {
+        $('.nav_screen').toggleClass('active');
+    });
+
+    $('.burger').click(function() {
+        $('.burger').toggleClass('active');
+    });
+
+    $('.burger').click(function() {
+        $('.header_body').toggleClass('active');
+    });
+
+    $('.questions_info_text_btn').click(function() {
+     event.preventDefault();
+     $('.pop_up_wrapper').addClass('active');
+    });
+
+    $('.pop_up_overlay').click(function() {
+        $('.pop_up_wrapper').removeClass('active');
+    });
+
+
+});
+
+
+// lang customise
+const lang = document.querySelectorAll('.lang_general');
+
+for (item of lang) {
+    item.addEventListener('click', function() {
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+        } else {
+            for (el of lang) {
+                el.classList.remove('active');
+            }
+            this.classList.add('active');
+        }
+    })
+}
+
+
+// menu_item customise
+const menu_item = document.querySelectorAll('.menu_item');
+
+for (item of menu_item) {
+    item.addEventListener('mouseenter', function() {
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+        } else {
+            for (el of menu_item) {
+                el.classList.remove('active');
+            }
+            this.classList.add('active');
+        }
+    })
+}
+
+
+
+// slider customise
+const slide = document.querySelectorAll('.slider_item');
+
+for (item of slide) {
+    item.addEventListener('mouseenter', function() {
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+        } else {
+            for (el of slide) {
+                el.classList.remove('active');
+            }
+            this.classList.add('active');
+        }
+    })
+}
